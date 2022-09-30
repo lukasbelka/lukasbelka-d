4b40edfd96b2989638e119168b9989dbf0dff06d
lukasbelka <lukasbelka@gmail.com>
2022-09-30 22:44:21 +0200

    Initial text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SK</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="main">
+
+    <h1>Hi, I'm Lukas, a German-based Analytics Developer.</h1>
+    <p>
+        Today it's my job to make data human-friendly and usable - I like it! However,
+        I was not always diving in data lakes why I would describe myself as kind of hybrid:
+        For nearly ten years I worked as a project manager in the financial services industry,
+        where I headed the PMO of a large IT transformation project for two years.
+        </p>
+    <p>
+        Back then I realized that dealing with data professionally would enable me to perfectly
+        combine domain knowledge with data and IT skills in order to gain insights that matter.
+        I have finished my MBA in eBusiness and started programming in Python and JavaScript - a perfect combination!
+    </p>
+    <p>
+        I believe open data is key to increase transparency and knowledge across all domains. Therefore
+        I use data, transform it and share it for free!
+    </p>
+</div>
+<style>
+
+div {
+    position: center
+    max-width 800px
+    padding 0 20 0 20
+    text-align center
+}
+</style>
