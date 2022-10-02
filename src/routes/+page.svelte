1bce3f5a439e1874e71d1c95a0d0b2fa0e745493
lukasbelka <lukasbelka@gmail.com>
2022-10-02 21:29:46 +0200

    Main css added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,15 @@
-<div class="main">
-
-    <h1>Hi, I'm Lukas, a German-based Analytics Developer.</h1>
-    <p>
-        Today it's my job to make data human-friendly and usable - I like it! However,
-        I was not always diving in data lakes why I would describe myself as kind of hybrid:
-        For nearly ten years I worked as a project manager in the financial services industry,
-        where I headed the PMO of a large IT transformation project for two years.
-        </p>
-    <p>
-        Back then I realized that dealing with data professionally would enable me to perfectly
-        combine domain knowledge with data and IT skills in order to gain insights that matter.
-        I have finished my MBA in eBusiness and started programming in Python and JavaScript - a perfect combination!
-    </p>
-    <p>
-        I believe open data is key to increase transparency and knowledge across all domains. Therefore
-        I use data, transform it and share it for free!
-    </p>
-</div>
-<style>
-
-.main {
-    margin: 20 auto;
-    max-width: 800px;
-    display: block;
-    text-align: center;
-}
-</style>
+<h1>Hi, I'm Lukas, a German-based Analytics Developer.</h1>
+<p>
+    Today it's my job to make data human-friendly and usable - I like it! However,
+    I was not always diving in data lakes why I would describe myself as kind of hybrid:
+    For nearly ten years I worked as a project manager in the financial services industry,
+    where I headed the PMO of a large IT transformation project for two years.
+</p>
+<p>
+    Back then I realized that dealing with data professionally would enable me to perfectly
+    combine domain knowledge with data and IT skills in order to gain insights that matter.
+    I have finished my MBA in eBusiness and started programming in Python and JavaScript - a perfect combination!
+</p>
+<p>
+    I believe open data is key to increase transparency and knowledge across all domains. Therefore
+    I use data, transform it and share it for free!</p>
