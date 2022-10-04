135d28492e327fc4ae6b0bdd52a1db33fcbfbd8a
lukasbelka <lukasbelka@gmail.com>
2022-10-04 11:55:15 +0200

    Fonts added Fonts added

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,29 +1,6 @@
 <script>
     import '../styles/global.css'
 </script>
-
-<header>
-
-</header>
-
 <main>
     <slot></slot>
 </main>
-
-<footer>
-    <p>Copyright</p>
-</footer>
-
-<style>
-header {
-    display: flex;
-    justify-content: center;
-}
-main {
-    max-width: 800px;
-    margin: 20px auto;
-}
-footer {
-    text-align: center;
-}
-</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,15 @@
-<h1>Hi, I'm Lukas, a German-based Analytics Developer.</h1>
-<p>
+<div class="section">
+
+    <h1>Hi, I'm Lukas, a German-based Analytics Developer.</h1>
+    <p>
     Today it's my job to make data human-friendly and usable - I like it! However,
-    I was not always diving in data lakes why I would describe myself as kind of hybrid:
-    For nearly ten years I worked as a project manager in the financial services industry,
+    I was not always wrangling data why I would describe myself as kind of hybrid:
+    For nearly ten years I worked as a project manager in the re-insurance industry,
     where I headed the PMO of a large IT transformation project for two years.
-</p>
-<p>
+        </p>
+    <p>
     Back then I realized that dealing with data professionally would enable me to perfectly
     combine domain knowledge with data and IT skills in order to gain insights that matter.
     I have finished my MBA in eBusiness and started programming in Python and JavaScript - a perfect combination!
-</p>
-<p>
-    I believe open data is key to increase transparency and knowledge across all domains. Therefore
-    I use data, transform it and share it for free!</p>
+    </p>
+</div>
