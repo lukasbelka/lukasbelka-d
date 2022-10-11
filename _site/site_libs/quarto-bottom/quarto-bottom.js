efc3931042c8b412f9d480283622b2fdecf3e846
lukasbelka <lukasbelka@gmail.com>
2022-10-11 15:05:47 +0200

    testing

---
+++ _site/site_libs/quarto-bottom/quarto-bottom.js
@@ -0,0 +1,21 @@
+const displayBottom = display `<style>
+.footer {
+  width: 100%;
+  padding: 0 0;
+  margin: 0 0;
+  background-color: #f1f1f1;
+  text-align: center;
+  border-radius: 4px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.p {
+  text-align: center;
+}
+</style>
+
+  <div class="footer">
+    <p>I dont </p>
+  </div>`
